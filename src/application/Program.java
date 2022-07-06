d2badb5ddf1800edc620ea57026e1aa717c7a9da
mpaulofg <mpaulofg@gmail.com>
2022-07-06 18:29:43 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -24,15 +24,21 @@ public class Program {
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
-		System.out.println("\n=== Test 4: seller findAll =====");
+		System.out.println("\n=== Test 3: seller findAll =====");
 		list = sellerDao.findAll();
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
-		System.out.println("\n=== Test 3: seller insert =====");
+		System.out.println("\n=== Test 4: seller insert =====");
 		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New id = " + newSeller.getId());
+		
+		System.out.println("\n=== Test 5: seller update =====");
+		seller = sellerDao.findById(1);
+		seller.setName("Martha Waine");
+		sellerDao.update(seller);
+		System.out.println("Update completed");
 		
 	}
 
